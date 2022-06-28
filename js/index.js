0fe7eb53834223b6a337a93d66b3d0d5502aeb8d
atralex <atralexrg@gmail.com>
2022-06-28 02:10:31 +0200

    links, animaciones

---
+++ js/index.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal();
+
+    ScrollReveal().reveal('.cabeza', {
+        duration: '3000',
+        origin: 'right',
+        distance: '-500px'
+    });
+    ScrollReveal().reveal('.titulo', {
+        duration: '3000',
+        origin: 'right',
+        distance: '1000px'
+    })
+    ScrollReveal().reveal('.imagen', {
+        duration: '3000'
+    })
